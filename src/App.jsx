5ecd40735ff158a261587bad10290e85cca66e26
Thenmozhi <jothi552004@gmail.com>
2024-07-14 00:15:20 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 import Nav from './components/Nav'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
      <Nav/>
      <Hero/>
      <Highlights/>
+     <Model/>
     </main>
   )
 }
